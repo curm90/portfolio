7af4a383534f6c22ae0cdfa57b6374bf1354f0c0
Liam Sutton <devliam90@gmail.com>
2020-04-14 15:39:27 +0100

    sidedrawer styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 import SideDrawer from './components/SideDrawer';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <SideDrawer />
+      <Backdrop />
     </div>
   );
 }
