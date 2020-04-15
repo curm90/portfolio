cb1676eadb1c2019c6279a999407122b450c88ae
Liam Sutton <devliam90@gmail.com>
2020-04-15 11:33:17 +0100

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => (
+  <section id='header' className='header-container'>
+    <div className='header-welcome-message'>
+      <h3 className='intro'>
+        Hi, I&apos;m <span className='name'>Liam Sutton</span>.
+      </h3>
+      <h3 className='intro'>I&apos;m a full stack web developer.</h3>
+      <a href='#projects'>
+        <button className='button'>View my work</button>
+      </a>
+    </div>
+  </section>
+);
+
+export default Header;
