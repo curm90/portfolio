ffe05932c20e1efc5016206a60453edd1cdfff68
Liam Sutton <devliam90@gmail.com>
2020-04-16 11:53:50 +0100

    about intro styles

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ const About = () => (
   <div id='about' className='about__container'>
     <h1 className='about__title'>ABOUT</h1>
     <div className='about__content'>
-      <div className='about__info'>
-        <p className='about__me'>
+      <div className='about-text__container'>
+        <p className='about-text'>
           Gamer - Reader - Located in England - Willing to relocate. I
           have a serious passion for learning all things programming,
           and creating seemless user experiences
