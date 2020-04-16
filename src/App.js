cfc9bc8bc2f2ef002ee7665d7284e907e4ae58b2
Liam Sutton <devliam90@gmail.com>
2020-04-16 13:20:17 +0100

    tablet about styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/About';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Navigation />
       <Header />
       <About />
--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,12 @@ import Skills from './Skills';
 const About = () => (
   <div id='about' className='about__container'>
     <h1 className='about__title'>ABOUT</h1>
-    <div className='about__content'>
-      <div className='about-text__container'>
-        <p className='about-text'>
-          Gamer - Reader - Located in England - Willing to relocate. I
-          have a serious passion for learning all things programming,
-          and creating seemless user experiences
-        </p>
-      </div>
+    <div className='about-text__container'>
+      <p className='about-text'>
+        Gamer - Reader - Located in England - Willing to relocate. I
+        have a serious passion for learning all things programming,
+        and creating seemless user experiences
+      </p>
     </div>
     <Skills />
   </div>
