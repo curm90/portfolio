77323d757e4140b4a3a488c6704dd69500bcf0a2
Liam Sutton <devliam90@gmail.com>
2020-04-16 17:40:05 +0100

    render projects html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
 import Footer from './components/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navigation />
       <Header />
       <About />
+      <Projects />
       <Footer />
     </div>
   );
