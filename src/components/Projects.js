6bdb57e391f493606df5bef730656bcee9a83662
Liam Sutton <devliam90@gmail.com>
2020-04-17 15:38:32 +0100

    add project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,6 @@ const Projects = () => {
     <section id='projects' className='projects__container'>
       <h1 className='projects__title'>PROJECTS</h1>
       <div className='project'>
-        <a
-          href='https://www.dev-coach.com'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <div className='project-image__container'></div>
-        </a>
         <div className='project__description'>
           <p className='description__text'>
             DevCoach is a platform that allows software developers to
@@ -21,34 +14,114 @@ const Projects = () => {
             problem.
           </p>
         </div>
-      </div>
-      <div className='project'>
-        <a href='https://weather-application-sutton.herokuapp.com/'>
-          <div className='project-image__container'></div>
-        </a>
-        <div className='project__description'>
-          <p className='description__text'>
-            DevCoach is a platform that allows software developers to
-            practice and enhance their technical interview skills.
-            With our built-in code editor and live video sharing, its
-            easy to connect with coaches and work through a coding
-            problem.
-          </p>
+        <div className='tech__container'>
+          <ul className='tech__list'>
+            <li className='tech'>
+              <i className='fab fa-react'></i>React
+            </li>
+            <li className='tech'>
+              <i class='fab fa-node-js'></i>Node
+            </li>
+            <li className='tech'>
+              <i class='far fa-comments'></i>Pusher
+            </li>
+            <li className='tech'>
+              <i class='far fa-file-code'></i>Judge0
+            </li>
+            <li className='tech'>
+              <i class='fas fa-photo-video'></i>Twilio
+            </li>
+          </ul>
+        </div>
+        <div className='project__links'>
+          <a
+            href='https://www.dev-coach.com'
+            className='project__link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Visit Website
+          </a>
+          <a
+            href='https://github.com/LABS-EU3/quality_hub_frontend'
+            className='project__link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Source Code
+          </a>
         </div>
       </div>
-
       <div className='project'>
-        <a href='https://chat-app-sutton.herokuapp.com/'>
-          <div className='project-image__container'></div>
-        </a>
         <div className='project__description'>
           <p className='description__text'>
-            DevCoach is a platform that allows software developers to
-            practice and enhance their technical interview skills.
-            With our built-in code editor and live video sharing, its
-            easy to connect with coaches and work through a coding
-            problem.
+            Need to know the weather forecast? Enter a location and
+            receive a summary of the current weather for that day.
           </p>
+        </div>
+        <div className='tech__container'>
+          <ul className='tech__list'>
+            <li className='tech'>
+              <i class='fab fa-node-js'></i>Node
+            </li>
+            <li className='tech'>
+              <i class='fas fa-bicycle'></i>hbs
+            </li>
+          </ul>
+        </div>
+        <div className='project__links'>
+          <a
+            href='https://weather-application-sutton.herokuapp.com/'
+            className='project__link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Visit Website
+          </a>
+          <a
+            href='https://github.com/curm90/NodeJs-weather-app'
+            className='project__link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Source Code
+          </a>
+        </div>
+      </div>
+      <div className='project'>
+        <div className='project__description'>
+          <p className='description__text'>
+            A real time chat app built using socket.io. Create a room
+            for you and your friends and meet new people.
+          </p>
+        </div>
+        <div className='tech__container'>
+          <ul className='tech__list'>
+            <li className='tech'>
+              <i class='fab fa-node-js'></i>Node
+            </li>
+            <li className='tech'>
+              <i class='far fa-comments'></i>Socket.io
+            </li>
+          </ul>
+        </div>
+        <div className='project__links'>
+          <a
+            href='https://chat-app-sutton.herokuapp.com/'
+            className='project__link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Visit Website
+          </a>
+          <a
+            href='https://github.com/curm90/Chat-App'
+            className='project__link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Source Code
+          </a>
         </div>
       </div>
     </section>
